126e577b27bbc1daa76952606010675d02e7e81b
poojashelke8 <poojabshelke0807@gmail.com>
2025-12-17 20:55:10 +0530

    odd string

--- Array/Pascal.py
+++ Array/Pascal.py
@@ -33,9 +33,40 @@ def getRow(rowIndex):
         
         return result[-1]
 
+import string
+
+def oddString(words):
+        letter = {letter: index for index, letter in enumerate(string.ascii_lowercase)}
+        res = []
+        for i in range(len(words)):
+            curr = []
+            n = len(words[i])
+            for j in range(n-1):
+                a = letter[words[i][j]]
+                b = letter[words[i][j+1]]
+                curr.append(b-a)
+            res.append(curr)
+        count = None
+        for i in range(len(res)):
+            count = 0
+            for j in range(len(res)):
+                if res[i] == res[j]:
+                    count += 1
+            if count == 1:
+                return words[i]
+        # return count
+        return res,words[count]
+
 
 res = generate(5)
 print(res,"Triangle")
 
 res2 = getRow(3)
 print(res2,"Triangle 2")
+
+words = ["adc","wzy","abc"]
+words2 = ["jijij","yxyxy","edede","edede","bbaaa","kjkjk"]
+# ["aaa","bob","ccc","ddd"]
+print(len(words[0]))
+val = oddString(words2)
+print(val,"values")
