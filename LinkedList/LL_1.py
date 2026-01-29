b77632063c2a0a93955b301f1a8378f69b48c5fb
poojashelke8 <poojabshelke0807@gmail.com>
2026-01-29 19:08:39 +0530

    insert in LL

--- LinkedList/LL_1.py
+++ LinkedList/LL_1.py
@@ -67,6 +67,25 @@ def insert_tail(head,val):
     temp.next = newNode
     return head
 
+def insert_kth_ele(head,val,k):
+    newNode = Node(val)
+    if head == None:
+        return None
+    if k == 1:
+        newNode.next = head
+        return newNode
+    cnt = 1
+    temp = head
+    prev = None
+    while temp:
+        if k == cnt:
+            prev.next = newNode
+            newNode.next = temp
+        prev = temp
+        temp = temp.next
+        cnt+=1 
+    return head
+
 # Driver code
 if __name__ == "__main__":
     arr = [2, 5, 8, 7]
@@ -99,6 +118,6 @@ if __name__ == "__main__":
     traversal_ll(head)
     head2 = insert_tail(head,200)
     insert_tail(head,500)
-
+    insert_kth_ele(head,250,2)
     traversal_ll(head2)
     print("Count -->", count)
